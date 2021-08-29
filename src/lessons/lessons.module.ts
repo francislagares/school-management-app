dac81ed4cf395d81b6198cd15dec452e682ae023
Francis Lagares <francis.lagares@gmail.com>
2021-08-29 19:06:16 +0200

    feat: create lessons service and createLesson method

--- src/lessons/lessons.module.ts
+++ src/lessons/lessons.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson.entity';
+import { LessonResolver } from './lesson.resolver';
+import { LessonsService } from './lessons.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Lesson])],
+  providers: [LessonResolver, LessonsService],
+})
 export class LessonsModule {}
