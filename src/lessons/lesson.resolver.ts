78c68575f8675a212e3d0732556fad9ab0f5b250
Francis Lagares <francis.lagares@gmail.com>
2021-08-29 21:43:46 +0200

    feat: create lessons resolver and getLessons service method

--- src/lessons/lesson.resolver.ts
+++ src/lessons/lesson.resolver.ts
@@ -1,4 +1,5 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { CreateLessonInput } from './lesson.input';
 import { LessonType } from './lesson.type';
 import { LessonsService } from './lessons.service';
 
@@ -6,22 +7,20 @@ import { LessonsService } from './lessons.service';
 export class LessonResolver {
   constructor(private lessonsService: LessonsService) {}
 
+  @Query((returns) => [LessonType])
+  lessons() {
+    return this.lessonsService.getLessons();
+  }
+
   @Query((returns) => LessonType)
-  lesson() {
-    return {
-      id: 'añsdlfkqñlk',
-      name: 'Physics Class',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  lesson(@Args('id') id: string) {
+    return this.lessonsService.getLesson(id);
   }
 
   @Mutation((returns) => LessonType)
   createLesson(
-    @Args('name') name: string,
-    @Args('startDate') startDate: string,
-    @Args('endDate') endDate: string,
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
   ) {
-    return this.lessonsService.createLesson(name, startDate, endDate);
+    return this.lessonsService.createLesson(createLessonInput);
   }
 }
