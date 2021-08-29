fbc58ed505c468f0561bfc22988a4ee15492d13c
Francis Lagares <francis.lagares@gmail.com>
2021-08-29 18:34:31 +0200

    feat: create lesson entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonsModule } from './lessons/lessons.module';
 import { StudentsModule } from './students/students.module';
+import { Lesson } from './lessons/lesson.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
