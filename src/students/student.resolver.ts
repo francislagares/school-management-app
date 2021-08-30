71622d9ce89e3b9f5e5514deb2afd1d7212cebc1
Francis Lagares <francis.lagares@gmail.com>
2021-08-30 21:46:53 +0200

    feat: create query to get students

--- src/students/student.resolver.ts
+++ src/students/student.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateStudentInput } from './student.input';
 import { StudentType } from './student.type';
 import { StudentsService } from './students.service';
@@ -6,6 +6,11 @@ import { StudentsService } from './students.service';
 @Resolver((of) => StudentType)
 export class StudentResolver {
   constructor(private studentService: StudentsService) {}
+
+  @Query((returns) => [StudentType])
+  async students() {
+    return this.studentService.getStudents();
+  }
 
   @Mutation((returns) => StudentType)
   async createStudent(
